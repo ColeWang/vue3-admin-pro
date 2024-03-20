292fcf7fea59fccbbbc43236208e927a925e38a8
cole <yiqing.wang@dreamisland.ai>
2024-03-20 19:35:13 +0800

    feat: useFloatForm

--- src/components/form/layouts/drawer-form/index.jsx
+++ src/components/form/layouts/drawer-form/index.jsx
@@ -1,79 +1,26 @@
 import { defineComponent, Fragment, ref, unref, watch } from 'vue'
 import { Drawer } from 'ant-design-vue'
-import { useLocaleReceiver } from '@/components/locale-provider'
 import { default as BaseForm, Submitter } from '../../base-form'
+import { default as useFloatForm, FloatProps } from '../hooks/useFloatForm'
+import { useLocaleReceiver } from '@/components/locale-provider'
 import { isFunction, pick } from 'lodash-es'
-
-const drawerFormProps = {
-    ...BaseForm.props,
-    ...Submitter.props,
-    layout: {
-        type: String,
-        default: 'vertical'
-    },
-    open: {
-        type: Boolean,
-        default: false
-    },
-    title: {
-        type: String,
-        default: undefined
-    },
-    width: {
-        type: Number,
-        default: 512
-    },
-    maskClosable: {
-        type: Boolean,
-        default: true
-    },
-    destroyOnClose: {
-        type: Boolean,
-        default: true
-    },
-    drawerProps: {
-        type: Object,
-        default: () => ({})
-    },
-    onOpen: {
-        type: Function,
-        default: undefined
-    },
-    onCancel: {
-        type: Function,
-        default: undefined
-    },
-    onAfterClose: {
-        type: Function,
-        default: undefined
-    },
-    onOpenChange: {
-        type: Function,
-        default: undefined
-    },
-    onLoadingChange: {
-        type: Function,
-        default: undefined
-    }
-}
 
 export default defineComponent({
     inheritAttrs: false,
-    props: drawerFormProps,
+    props: FloatProps,
     emits: ['update:open', 'open', 'cancel', 'afterClose', 'openChange', 'loadingChange'],
     setup (props, { emit, slots, attrs, expose }) {
         const baseFormRef = ref(null)
 
         const { t } = useLocaleReceiver('Form')
 
-        const sOpen = ref(props.open)
-        const loading = ref(false)
+        const { open, loading, onOpen, onCancel, onFinish } = useFloatForm(props, {
+            onOpen: () => emit('open'),
+            onCancel: () => emit('cancel'),
+            onUpdateOpen: (value) => emit('update:open', value)
+        })
 
-        watch(() => props.open, (value) => {
-            sOpen.value = value
-        }, { immediate: true })
-
-        watch(sOpen, (value) => {
+        watch(open, (value) => {
             emit('openChange', value)
         })
 
@@ -81,35 +28,17 @@ export default defineComponent({
             emit('loadingChange', value)
         })
 
-        function setOpenValue (value) {
-            sOpen.value = value
-            emit('update:open', value)
-        }
-
-        function onOpenClick () {
-            setOpenValue(true)
-            props.onOpen && emit('open')
-            isFunction(props.drawerProps.onOpen) && props.drawerProps.onOpen()
-        }
-
-        function onClose () {
-            if (unref(loading)) return
-            setOpenValue(false)
-            props.onCancel && emit('cancel')
-            isFunction(props.drawerProps.onCancel) && props.drawerProps.onCancel()
-        }
-
         function onAfterClose () {
             const drawerProps = {
                 ...pick(props, Object.keys(Drawer.props)),
-                ...props.drawerProps
+                ...props.extraProps
             }
             if (drawerProps.destroyOnClose) {
                 const context = unref(baseFormRef)
                 context && context.resetFields()
             }
             props.onAfterClose && emit('afterClose')
-            isFunction(props.drawerProps.onAfterClose) && props.drawerProps.onAfterClose()
+            isFunction(props.extraProps.onAfterClose) && props.extraProps.onAfterClose()
         }
 
         function onSubmit () {
@@ -117,75 +46,53 @@ export default defineComponent({
             context && context.submit()
         }
 
-        async function onFinishHandle (values) {
-            const request = props.onFinish || props.onSubmit
-            if (!isFunction(request) || unref(loading)) return
-            loading.value = true
-            try {
-                const result = await request(values)
-                loading.value = false
-                result && onClose()
-            } catch (err) {
-                loading.value = false
-                console.warn(err)
-            }
-        }
-
         function getFormInstance () {
             return unref(baseFormRef)
         }
 
         expose({
-            open: onOpenClick,
-            close: onClose,
+            open: onOpen,
+            close: onCancel,
             getFormInstance,
         })
 
         return () => {
-            const { drawerProps, submitText, resetText } = props
-            const { submitter: submitterSlot, ...restSlots } = slots
+            const { extraProps, submitText, resetText } = props
 
             const drawerSlots = {
-                default: () => {
-                    const baseFormProps = {
-                        ...attrs,
-                        ...pick(props, Object.keys(BaseForm.props)),
-                        onSubmit: onFinishHandle,
-                        onFinish: onFinishHandle
-                    }
-                    return (
-                        <BaseForm {...baseFormProps} ref={baseFormRef} v-slots={restSlots}/>
-                    )
-                },
                 extra: () => {
                     const submitterProps = {
                         ...pick(props, Object.keys(Submitter.props)),
-                        loading: unref(loading),
                         submitText: submitText || t('okText'),
                         resetText: resetText || t('cancelText'),
+                        loading: unref(loading),
                         onSubmit: onSubmit,
-                        onReset: onClose
-                    }
-                    if (submitterSlot && isFunction(submitterSlot)) {
-                        return submitterSlot(submitterProps)
+                        onReset: onCancel
                     }
                     return (
                         <Submitter {...submitterProps}/>
+                    )
+                },
+                default: () => {
+                    const originProps = { ...attrs, ...pick(props, Object.keys(BaseForm.props)) }
+                    const baseFormProps = { ...originProps, onFinish: onFinish }
+                    return (
+                        <BaseForm {...baseFormProps} ref={baseFormRef} v-slots={slots}/>
                     )
                 }
             }
 
             const triggerDom = slots.trigger && (
-                <div style={{ display: 'inline-block' }} onClick={onOpenClick}>
+                <div style={{ display: 'inline-block' }} onClick={onOpen}>
                     {slots.trigger()}
                 </div>
             )
 
             const needDrawerProps = {
                 ...pick(props, Object.keys(Drawer.props)),
-                ...drawerProps,
-                open: unref(sOpen),
-                onClose: onClose,
+                ...extraProps,
+                open: unref(open),
+                onClose: onCancel,
                 onAfterOpenChange: onAfterClose
             }
 
---
+++ src/components/form/layouts/hooks/useFloatForm.js
@@ -0,0 +1,108 @@
+import { ref, unref, watch } from 'vue'
+import { default as BaseForm, Submitter } from '../../base-form'
+import { isFunction } from 'lodash-es'
+import { tryOnScopeDispose } from '@/utils/index.js'
+
+export const FloatProps = {
+    ...BaseForm.props,
+    ...Submitter.props,
+    layout: {
+        type: String,
+        default: 'vertical'
+    },
+    open: {
+        type: Boolean,
+        default: false
+    },
+    title: {
+        type: String,
+        default: undefined
+    },
+    width: {
+        type: Number,
+        default: 512
+    },
+    maskClosable: {
+        type: Boolean,
+        default: true
+    },
+    destroyOnClose: {
+        type: Boolean,
+        default: true
+    },
+    extraProps: {
+        type: Object,
+        default: () => ({})
+    },
+    onOpen: {
+        type: Function,
+        default: undefined
+    },
+    onCancel: {
+        type: Function,
+        default: undefined
+    },
+    onAfterClose: {
+        type: Function,
+        default: undefined
+    },
+    onOpenChange: {
+        type: Function,
+        default: undefined
+    },
+    onLoadingChange: {
+        type: Function,
+        default: undefined
+    }
+}
+
+function useFloatForm (props, options) {
+    const open = ref(props.open)
+    const loading = ref(false)
+
+    const stopWatchOpen = watch(() => props.open, (value) => {
+        open.value = value
+    }, { immediate: true })
+
+    function setOpenValue (value) {
+        open.value = value
+        options.onUpdateOpen && options.onUpdateOpen(value)
+    }
+
+    function onOpen () {
+        setOpenValue(true)
+        options.onOpen && options.onOpen()
+        isFunction(props.extraProps.onOpen) && props.extraProps.onOpen()
+    }
+
+    function onCancel () {
+        if (unref(loading)) return
+        setOpenValue(false)
+        options.onCancel && options.onCancel()
+        isFunction(props.extraProps.onCancel) && props.extraProps.onCancel()
+    }
+
+    async function onFinish (values) {
+        const request = props.onFinish || props.onSubmit
+        if (!isFunction(request) || unref(loading)) return
+        loading.value = true
+        try {
+            const result = await request(values)
+            loading.value = false
+            result && onCancel()
+        } catch (err) {
+            loading.value = false
+            console.warn(err)
+        }
+    }
+
+    function onStop () {
+        stopWatchOpen && stopWatchOpen()
+    }
+
+    tryOnScopeDispose(onStop)
+
+    return { open, loading, onOpen, onCancel, onFinish }
+}
+
+export default useFloatForm
