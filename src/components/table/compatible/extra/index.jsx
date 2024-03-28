1b187f616354df4e6b3ed6e38230043642167d51
cole <yiqing.wang@dreamisland.ai>
2024-03-28 16:25:46 +0800

    fix: extra

--- src/components/table/compatible/extra/index.jsx
+++ src/components/table/compatible/extra/index.jsx
@@ -1,23 +1,16 @@
 import { defineComponent } from 'vue'
-import { Card } from 'ant-design-vue'
+import classNames from '@/utils/classNames/bind'
+import styles from './style/index.module.scss'
+
+const cx = classNames.bind(styles)
 
 export default defineComponent({
     inheritAttrs: false,
     setup (props, { slots }) {
         return () => {
-            const cardProps = {
-                bodyStyle: {
-                    paddingBlock: '24px',
-                    paddingInline: '24px'
-                },
-                style: {
-                    marginBottom: '16px'
-                }
-            }
-
             const children = slots.default && slots.default()
 
-            return <Card {...cardProps}>{children}</Card>
+            return <div class={cx('extra')}>{children}</div>
         }
     }
 })
