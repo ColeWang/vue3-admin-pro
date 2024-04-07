6cf88249fd5d91470c36867bf71bcd6dffe570a1
cole <yiqing.wang@dreamisland.ai>
2024-04-07 15:00:34 +0800

    测试

---
+++ public/test/assets/index-iHXqDcmQ.js
@@ -0,0 +1,1 @@
+import{L as t}from"./index-DLfVr0bV.js";import"./index-mDHzngDI.js";import{d as e,j as o,G as r}from"./vue-DS5iLM9r.js";import"./library-DpjK_rHT.js";import"./plugins-BvTQrPWK.js";const n=e({inheritAttrs:!1,setup(){return()=>o(t,{toolbar:!1},{default:()=>[r("Deposit")]})}});export{n as default};
