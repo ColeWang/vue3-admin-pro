ada59b34e81c8caab260e599e30f9df44fc84274
cole <yiqing.wang@dreamisland.ai>
2024-06-07 19:00:49 +0800

    chore: version 0.2.1

--- entry.packages.js
+++ entry.packages.js
@@ -2,11 +2,14 @@ import Loading from '@/components/loading'
 import Sidebar from '@/layout/components/sidebar'
 import TagsNav from '@/layout/components/tags-nav'
 import Navbar from '@/layout/components/navbar'
+import BaseField, { CustomFields } from '@/components/base-field'
 
 export { Loading }
 export * from '@/components/icon'
 export * from '@/components/table'
 export * from '@/components/form'
+export * from '@/components/descriptions'
 export { Sidebar }
 export { Navbar }
 export { TagsNav }
+export { BaseField, CustomFields }
