d3751e17d8cd1fb7312ec54b0a77028dc07707bf
cole <yiqing.wang@dreamisland.ai>
2024-06-12 11:15:02 +0800

    docs: 多语言

--- src/locale/en-US/routes.js
+++ src/locale/en-US/routes.js
@@ -3,8 +3,14 @@ export default {
     Form: 'Form',
     FormLayout: 'Form Layout',
     FormModal: 'Modal Form',
+    FormDrawer: 'Drawer Form',
     FormFilter: 'Filter Form',
     Table: 'Table',
     TableIndex: 'Table',
-    TableCustomSearch: 'Custom Search'
+    TableCustomSearch: 'Custom Search',
+    TableEditableTable: 'Editable Table',
+    Descriptions: 'Descriptions',
+    DescriptionsIndex: 'Descriptions',
+    Test: 'Test',
+    TestIndex: 'Test'
 }
--- src/locale/zh-CN/routes.js
+++ src/locale/zh-CN/routes.js
@@ -3,8 +3,14 @@ export default {
     Form: '表单',
     FormLayout: '表单布局',
     FormModal: '浮层表单',
+    FormDrawer: '抽屉表单',
     FormFilter: '筛选表单',
     Table: '表格',
     TableIndex: '表格',
-    TableCustomSearch: '自定义搜索'
+    TableCustomSearch: '自定义搜索',
+    TableEditableTable: '可编辑表格',
+    Descriptions: '描述列表',
+    DescriptionsIndex: '描述列表',
+    Test: '测试',
+    TestIndex: '测试'
 }
