eaab6d7c5c601144d9f191ee3c6cee71e7a05d07
cole <yiqing.wang@dreamisland.ai>
2024-06-14 11:23:09 +0800

    workflows: coverage

--- vitest.config.js
+++ vitest.config.js
@@ -6,6 +6,25 @@ export default defineConfig((configEnv) => {
         test: {
             environment: 'jsdom',
             include: ['**/*.{test,spec,type-test}.{js,mjs,cjs,ts,tsx,jsx}'],
+            coverage: {
+                exclude: [
+                    'src/assets/**',
+                    'src/boot/**',
+                    'src/components/**',
+                    'src/config/**',
+                    'src/css/**',
+                    'src/layout/**',
+                    'src/locale/**',
+                    'src/permission/**',
+                    'src/router/**',
+                    'src/stores/**',
+                    'src/utils/**',
+                    'src/views/**',
+                    'src/App.jsx',
+                    'src/main.js',
+                    'src/useAppInstance.js',
+                ]
+            }
         }
     }))
 })
