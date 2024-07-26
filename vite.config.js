22f097bddf5377f293e23869d0d903e8497c201e
cole <cole@google.com>
2024-07-26 13:05:57 +0800

    fix: manualChunks

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
 import { fileURLToPath } from 'node:url'
 import { dirname, resolve } from 'node:path'
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, isCSSRequest, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { reduce } from 'lodash-es'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+
+const vendors = reduce({
+    'vue-vendor': ['vue', 'vue-router', 'pinia'],
+    'antd-vendor': ['ant-design-vue', '@ant-design/icons-vue'],
+    'utils-vendor': ['dayjs', 'lodash-es'],
+    'common-vendor': ['axios', 'vue-i18n', 'js-cookie', 'nprogress']
+}, (result, modules, key) => {
+    const needVendors = modules.map((name) => {
+        return { name: name, vendor: key }
+    })
+    return [...result, ...needVendors]
+}, [])
+
+function manualChunks (id) {
+    if (id.includes('/node_modules/') && !isCSSRequest(id)) {
+        const result = vendors.find((item) => {
+            return id.includes(`/node_modules/${item.name}/`)
+        })
+        return result ? result.vendor : undefined
+    }
+    return undefined
+}
 
 export default defineConfig((config) => {
     const env = loadEnv(config.mode, __dirname, ['VITE_', 'ENV_'])
@@ -23,12 +46,7 @@ export default defineConfig((config) => {
         build: {
             rollupOptions: {
                 output: {
-                    manualChunks: {
-                        'vue-vendor': ['vue', 'vue-router', 'pinia'],
-                        'antd-vendor': ['ant-design-vue', '@ant-design/icons-vue'],
-                        'utils-vendor': ['dayjs', 'lodash-es'],
-                        'common-vendor': ['axios', 'vue-i18n', 'js-cookie', 'nprogress']
-                    }
+                    manualChunks: manualChunks
                 }
             }
         },
