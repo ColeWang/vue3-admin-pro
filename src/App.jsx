d74a2c2babadb47d619980c3fb6131d6a08b7006
cole <cole@google.com>
2024-07-29 01:23:09 +0800

    docs: app config

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,14 @@ import { createAppInstance } from './useAppInstance'
 export default defineComponent({
     inheritAttrs: false,
     setup () {
+        /**
+         * 实际开发中，可能不需要【多语言、定制主题】等。为方便删除相关逻辑、以及理解数据流向。没有在这里初始化，可能会导致一些问题。
+         * 例如非 Layout 组件内没有多语言、定制主题等。
+         *
+         * 如有需要，请把初始化相关逻辑迁移到 App.jsx
+         * src/layout/components/language/index.jsx
+         * src/layout/components/settings/ThemeSettings.jsx
+         */
         const localeMessage = ref({})
 
         const theme = ref('dark')
