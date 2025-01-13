e88bf18c248caca35b0f70cd3589662539cc7166
cole <cole@google.com>
2025-01-13 18:23:55 +0800

    chore: site-pro

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+// 缓存
+let globalProperties = null
+
 const instance = axios.create({
     // baseURL: genBaseUrl(location.href),
     timeout: 5000,
@@ -27,6 +30,8 @@ instance.interceptors.response.use((res) => {
 // 这将允许您使用 this.$axios (Vue Options API形式)
 export default ({ app }) => {
     app.config.globalProperties.$axios = instance
+    // 缓存 可以获取 $router、$i18n 等
+    globalProperties = app.config.globalProperties
 }
 
 export { instance as request }
